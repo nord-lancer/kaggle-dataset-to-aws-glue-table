f2cf07303036d331dbdc475b0265f0d99b8f67b7
Ivan O <ivan.ostapchuk.27@gmail.com>
2024-03-18 19:37:32 +0200

    Add function for getting dataset files list
    And other minor changes.

--- src/lambdas/save_kaggle_dataset_lambda.py
+++ src/lambdas/save_kaggle_dataset_lambda.py
@@ -5,9 +5,12 @@ import logging
 from kaggle import KaggleApi
 
 
+KAGGLE_DATASET_NAME_SLASH = '/'
+
+
 logger = logging.getLogger("Lambda")
 logging.basicConfig()
-logs_level = "INFO"
+logs_level = "DEBUG"
 logger.setLevel(logs_level)
 logger.info(f"Logger initialized. Set level to: {logs_level}")
 
@@ -19,19 +22,28 @@ def find_dataset(kaggle_api,dataset_name):
         logger.info(f"Found {len(kaggle_dataset)} datasets:")
         for dataset in kaggle_dataset:
             logger.info(dataset)
-        logger.error("ERROR: This API can process only one dataset at a time!")  # TODO: return this message
-        logger.error("Please input the exact name of the dataset like this: username/dataset-name.")
+        logger.error("ERROR: This API can process only one dataset at a time!")  # TODO: return this message from the API
+        logger.error("Please input the exact name of the dataset like this: "
+                     "username/dataset-name.")
         return 1
     else:
         logger.info(f"Found dataset {kaggle_dataset}!")
-        return kaggle_dataset
+        logger.debug(f"Dataset type: {type(kaggle_dataset)}")
+        return kaggle_dataset[0]  # return a dataset object instead of a list with 1 object in it
     
 
-def get_dataset_files_list(kaggle_api, dataset_name):
-    dataset_name = dataset_name[0].append('/')
-    print(dataset_name)
-    kaggle_dataset_files_list = kaggle_api.dataset_list_files(dataset_name)
-    logger.info(f"Kaggle dataset status: {kaggle_dataset_status}")
+def get_dataset_files_list(kaggle_api, dataset):
+    # TODO: check for error message before returning the file list?
+    dataset_full_name = (dataset.ref
+                         + KAGGLE_DATASET_NAME_SLASH
+                         + str(dataset.currentVersionNumber))
+    kaggle_dataset_files_list = kaggle_api.dataset_list_files(dataset_full_name)
+    if kaggle_dataset_files_list.error_message:
+        logger.error(f"When retrieving kaggle dataset files list, received "
+                     f"the following error message: "
+                     f"{kaggle_dataset_files_list.error_message}")
+    logger.info(f"Kaggle dataset files: {kaggle_dataset_files_list.files}")
+    return kaggle_dataset_files_list.files
 
 
 def get_dataset_metadata(kaggle_api,dataset_name):
@@ -47,16 +59,23 @@ def load_dataset_metadata_to_s3():
     pass
 
 
-def lambda_handler(event, context):
+def get_authenticated_kaggle_api_obj():
     kaggle_api_obj = KaggleApi()
     kaggle_api_obj.authenticate()
+    return kaggle_api_obj
 
-    dataset_to_retrieve = event['dataset_search_name']
-    dataset_name = find_dataset(kaggle_api_obj, dataset_to_retrieve)
-    logger.info(f"{type(dataset_name)}")
-    logger.info(f"{dataset_name}")
+
+def lambda_handler(event, context):
+    kaggle_api = get_authenticated_kaggle_api_obj()
+
+    dataset = find_dataset(kaggle_api, event['dataset_search_name'])
     
-    get_dataset_files_list(kaggle_api_obj, dataset_name)
+    logger.info(f"Owner name: {dataset.ownerName}")
+    logger.debug(f"Current version: {dataset.currentVersionNumber}")
+    logger.info(f"Dataset size: {dataset.size}")
+    logger.debug(f"Dataset size in bytes: {dataset.totalBytes}")
+    
+    dataset_files_list = get_dataset_files_list(kaggle_api, dataset)
      
 
 def main():
@@ -69,8 +88,8 @@ if __name__ == '__main__':
     main()
 
 # algorithm:
-# 1. check if only one dataset exists by given name
-# 2. get dataset metadata
+# 1. check if only one dataset exists by given name (done: find_dataset func)
+# 2. get dataset metadata (file list, anything else?)
 # 3. check if dataset is structured (API will only handle structured datasets)
 # 4. check if dataset is less than 80MB (for now)
 # 5. check if event has S3 location for data
